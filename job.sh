9dd58b3373b30d5e45615b456ad37c487fb5a7a7
Amina LEM <45292279+AminaLEM@users.noreply.github.com>
2023-03-20 15:09:43 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -13,15 +13,6 @@
 #srun snakemake --cores --unlock
 
 # # get prediction output with all genes in the interesction of DESeq2 and edgeR
-# srun snakemake --cores all predict --snakefile src/snakefile   --configfile config-All.yml --rerun-incomplete
-# srun snakemake --cores all predict_val --snakefile src/snakefile   --configfile config-All.yml --rerun-incomplete
-# # get prediction output with the interesction of TOP15 genes from DESeq2 and edgeR
-# srun snakemake --cores all predict --snakefile src/snakefile   --configfile config-Top15.yml --rerun-incomplete
-# srun snakemake --cores all predict_val --snakefile src/snakefile   --configfile config-Top15.yml --rerun-incomplete
-# # get prediction output with the interesction of TOP10 genes from DESeq2 and edgeR
-# srun snakemake --cores all predict --snakefile src/snakefile   --configfile config-Top10.yml --rerun-incomplete
-# srun snakemake --cores all predict_val --snakefile src/snakefile   --configfile config-Top10.yml --rerun-incomplete
-# # get prediction output with the interesction of TOP5 genes from DESeq2 and edgeR
 # srun snakemake --cores all predict --snakefile src/snakefile   --configfile config-Top5.yml --rerun-incomplete
 # srun snakemake --cores all predict_val --snakefile src/snakefile   --configfile config-Top5.yml --rerun-incomplete
 # # get prediction output with the interesction of TOP5 genes from DESeq2 and edgeR and GenderAge integration
@@ -38,35 +29,6 @@
 
 
 ##### With singularity
-# singularity run -B /prj/NUM_CODEX_PLUS/Amina/CellSubmission -B /home/alemsara \
-# /prj/NUM_CODEX_PLUS/Amina/CellSubmission/Prediction_scOmics/singularity/aminale_immun2sev_latest-2023-02-28-3349561f6f7d.sif \
-#                   "snakemake --cores all predict --snakefile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/src/snakefile  \
-#                   --configfile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/config-All.yml --directory /home/alemsara"
-
-# singularity run -B /prj/NUM_CODEX_PLUS/Amina/CellSubmission -B /home/alemsara \
-# /prj/NUM_CODEX_PLUS/Amina/CellSubmission/Prediction_scOmics/singularity/aminale_immun2sev_latest-2023-02-28-3349561f6f7d.sif \
-#                   "snakemake --cores all predict_val --snakefile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/src/snakefile  \
-#                   --configfile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/config-All.yml --directory /home/alemsara"
-                  
-# singularity run -B /prj/NUM_CODEX_PLUS/Amina/CellSubmission -B /home/alemsara \
-# /prj/NUM_CODEX_PLUS/Amina/CellSubmission/Prediction_scOmics/singularity/aminale_immun2sev_latest-2023-02-28-3349561f6f7d.sif \
-#                   "snakemake --cores all predict --snakefile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/src/snakefile  \
-#                   --configfile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/config-Top15.yml --directory /home/alemsara"
-
-# singularity run -B /prj/NUM_CODEX_PLUS/Amina/CellSubmission -B /home/alemsara \
-# /prj/NUM_CODEX_PLUS/Amina/CellSubmission/Prediction_scOmics/singularity/aminale_immun2sev_latest-2023-02-28-3349561f6f7d.sif \
-#                   "snakemake --cores all predict_val --snakefile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/src/snakefile  \
-#                   --configfile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/config-Top15.yml --directory /home/alemsara"
-
-# singularity run -B /prj/NUM_CODEX_PLUS/Amina/CellSubmission -B /home/alemsara \
-# /prj/NUM_CODEX_PLUS/Amina/CellSubmission/Prediction_scOmics/singularity/aminale_immun2sev_latest-2023-02-28-3349561f6f7d.sif \
-#                   "snakemake --cores all predict --snakefile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/src/snakefile  \
-#                   --configfile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/config-Top10.yml --directory /home/alemsara"
-
-# singularity run -B /prj/NUM_CODEX_PLUS/Amina/CellSubmission -B /home/alemsara \
-# /prj/NUM_CODEX_PLUS/Amina/CellSubmission/Prediction_scOmics/singularity/aminale_immun2sev_latest-2023-02-28-3349561f6f7d.sif \
-#                   "snakemake --cores all predict_val --snakefile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/src/snakefile  \
-#                   --configfile /prj/NUM_CODEX_PLUS/Amina/CellSubmission/new_runs/ImmunOMICS/config-Top10.yml --directory /home/alemsara"
 
 singularity run -B /prj/NUM_CODEX_PLUS/Amina/CellSubmission -B /home/alemsara \
 /prj/NUM_CODEX_PLUS/Amina/CellSubmission/Prediction_scOmics/singularity/aminale_immun2sev_latest-2023-02-28-3349561f6f7d.sif \
